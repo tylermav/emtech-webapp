dc4287136f70fcc40822f0de79bac077ba207034
ichou1544c <caasichou@gmail.com>
2019-10-19 23:46:51 -0500

    Update server.js

--- emtech-local/emtech-app/routes/server.js
+++ emtech-local/emtech-app/routes/server.js
@@ -18,4 +18,5 @@ var server = app.listen(8081, function () {
    var port = server.address().port
 
    console.log("Example app listening at http://%s:%s", host, port)
+   zabo.users.create(account)
 })
